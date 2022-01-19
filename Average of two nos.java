dbe87222ff668e46a3b83ab9330ab4293ab35eac
Ashwani Kumar Sonu <94763917+AshwaniKrSonu619@users.noreply.github.com>
2022-01-19 20:11:51 +0530

    DSA

---
+++ Average of two nos.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+      
+      Scanner scn = new Scanner(System.in);
+      
+      int a = scn.nextInt();
+      int b = scn.nextInt();
+      
+      int ans = (a+b)/2;
+      
+      System.out.println(ans);
+   
+   
+
+  }
+}
