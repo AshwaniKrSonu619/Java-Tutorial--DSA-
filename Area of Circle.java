f3c2abcdac3926a5c401ceeab6856a2ec24ca33e
Ashwani Kumar Sonu <94763917+AshwaniKrSonu619@users.noreply.github.com>
2022-01-21 20:04:18 +0530

    DSA

---
+++ Area of Circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+
+Scanner scn = new Scanner(System.in);
+     int radius = scn.nextInt();
+     
+     System.out.println(3.314*(radius*radius));
+ }    
+}     
