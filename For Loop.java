6db4814baabff4bd86ca954bc2426ed7eb6e3016
Ashwani Kumar Sonu <94763917+AshwaniKrSonu619@users.noreply.github.com>
2022-04-22 16:03:52 +0530

    syntax edited

--- For Loop.java
+++ For Loop.java
@@ -3,7 +3,7 @@ import java.util.*;
 
     public static void main(String[] args){
         
-    for (int a=1; a<=10; a=a+1){   // syntax    for (starting; endind;  a=a+1){
+    for (int a=1; a<=10; a=a+1){   // syntax    for (starting; condition;  manipulate){
         System.out.print(a);
     }
   }
